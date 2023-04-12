1badb91c31874cdf2bd6e2f5d91c6c45f1435891
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-04-12 20:36:56 +0900

    update

--- test.rb
+++ test.rb
@@ -6,7 +6,30 @@ def bigger(a, b)
   end
 end
 
-a = 1
-b = 2
+def biggest(a, b, c)
+  bigger_one = bigger(a, b)
+  if bigger_one == a
+    biggest = bigger(a,c)
+  elsif bigger_one == b
+    biggest = bigger(b,c)
+  end
+  biggest
+end
 
-puts bigger(a, b)
+def median(a, b, c)
+  
+  biggest_number = biggest(a, b, c)
+
+  if biggest_number == a
+    median_number = bigger(b, c)
+  elsif biggest_number == b
+    median_number = bigger(a, c)
+  elsif biggest_number == c
+    median_number = bigger(a, b)
+  end
+  median_number
+end
+
+puts median(5,7,2)
+puts median(7,3,2)
+puts median(1,4,2)
