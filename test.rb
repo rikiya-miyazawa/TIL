0f3ce013d04315cf02ed43a42f6a0e9c4ebb61b2
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-05-02 10:42:55 +0900

    update

--- test.rb
+++ test.rb
@@ -1,7 +1,9 @@
-def default_args(a, b, c = 0, d = 0)
-  puts "a=#{a}, b=#{b}, c=#{c}, d=#{d}"
+def foo(time = Time.now, message = bar)
+  puts "time: #{time}, message: #{message}"
 end
 
-default_args(1,2)
-default_args(1,2,3)
-default_args(1,2,3,4)
+def bar
+  'BAR'
+end
+
+foo
